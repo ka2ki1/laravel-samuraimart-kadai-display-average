0b8496177f61226813c7089ed8a018ad7f6e5ab5
ka2ki1 <keshupinglin@gmail.com>
2024-04-19 22:40:18 +0900

    提出

---
+++ app/Admin/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Routing\Router;
+use App\Admin\Controllers\CategoryController;
+use App\Admin\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+use Encore\Admin\Facades\Admin;
+
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('categories', CategoryController::class);
+    $router->resource('products', ProductController::class);
+
+});
+
+
